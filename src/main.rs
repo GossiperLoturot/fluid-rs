7e9f6ebb1b586514f862cfb90200a10fb7c475cf
GossiperLoturot <ts20030211@gmail.com>
2023-06-19 21:27:40 +0900

    :recycle: refactor: format and add comment

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
 use glam::*;
 use rand::Rng;
 
+// simulation params
+const ITERATE: usize = 256;
+const TIME_STEP: f32 = 0.0001;
 const PARTICLE_SIZE: usize = 256;
 const PARTICLE_RADIUS: f32 = 0.1;
 const PARTICLE_MASS: f32 = 1.0;
+
+// viscosity params
 const VISCOSITY: f32 = 0.15;
+
+// pressure params
 const PRESSURE_STIFFNESS: f32 = 50.0;
 const REST_DENSITY: f32 = 500.0;
-const TIME_STEP: f32 = 0.0001;
+
+// bounds params
 const VELOCITY_DUMPING: f32 = -0.5;
-const GRAVITY_ACCELERATION: f32 = 9.8;
 const RANGE_X: f32 = 1.0;
 const RANGE_Y: f32 = 1.0;
+
+// renderer params
+const STDOUT_X: usize = 32;
+const STDOUT_Y: usize = 16;
+
+// constant
 const PI: f32 = std::f32::consts::PI;
-const ITERATE: usize = 256;
+const GRAVITY_ACCELERATION: f32 = 9.8;
 
 #[derive(Debug, Default, Clone, Copy)]
 struct Particle {
     position: Vec2,
     velocity: Vec2,
+    acceleration: Vec2,
     density: f32,
     pressure: f32,
-    acceleration: Vec2,
 }
+
+// kernel functions
 
 fn poly6(r: Vec2, h: f32) -> f32 {
     if r.length() < h {
@@ -48,6 +63,7 @@ fn grad_spiky(r: Vec2, h: f32) -> Vec2 {
     }
 }
 
+// pipeline functions
 // refered from "Becker and Teschner, Weakly compressible SPH for free surface flows, SCA2007."
 
 fn compute_density_and_pressure(particles: &mut [Particle]) {
@@ -59,6 +75,7 @@ fn compute_density_and_pressure(particles: &mut [Particle]) {
                 continue;
             }
 
+            // density
             particles[i].density += PARTICLE_MASS
                 * poly6(
                     particles[i].position - particles[j].position,
@@ -66,6 +83,7 @@ fn compute_density_and_pressure(particles: &mut [Particle]) {
                 );
         }
 
+        // pressure
         particles[i].pressure =
             PRESSURE_STIFFNESS * ((particles[i].density / REST_DENSITY).powi(7) - 1.0).max(0.0);
     }
@@ -80,19 +98,7 @@ fn compute_acceleration(particles: &mut [Particle]) {
                 continue;
             }
 
-            // viscosity force
-            let acceleration =
-                VISCOSITY * PARTICLE_MASS * (particles[j].velocity - particles[i].velocity)
-                    / (particles[i].density + particles[j].density)
-                    * lap_viscocity(
-                        particles[i].position - particles[j].position,
-                        PARTICLE_RADIUS,
-                    );
-            if !acceleration.is_nan() {
-                particles[i].acceleration += acceleration;
-            }
-
-            // pressure force
+            // pressure
             let acceleration = -PARTICLE_MASS
                 * (particles[i].pressure / particles[i].density.powi(2)
                     + particles[j].pressure / particles[j].density.powi(2))
@@ -104,17 +110,31 @@ fn compute_acceleration(particles: &mut [Particle]) {
                 particles[i].acceleration += acceleration;
             }
 
+            // viscosity
+            let acceleration =
+                VISCOSITY * PARTICLE_MASS * (particles[j].velocity - particles[i].velocity)
+                    / (particles[i].density + particles[j].density)
+                    * lap_viscocity(
+                        particles[i].position - particles[j].position,
+                        PARTICLE_RADIUS,
+                    );
+            if !acceleration.is_nan() {
+                particles[i].acceleration += acceleration;
+            }
         }
 
+        // gravity
         particles[i].acceleration += Vec2::NEG_Y * GRAVITY_ACCELERATION;
     }
 }
 
 fn compute_position_and_velocity(particles: &mut [Particle]) {
     for i in 0..PARTICLE_SIZE {
+        // forward euler method
         particles[i].velocity += particles[i].acceleration * TIME_STEP;
         particles[i].position += particles[i].velocity * TIME_STEP;
 
+        // bounds
         if particles[i].position.x < 0.0 {
             particles[i].position.x = 0.0;
             particles[i].velocity.x *= VELOCITY_DUMPING;
@@ -131,6 +151,8 @@ fn compute_position_and_velocity(particles: &mut [Particle]) {
         }
     }
 }
+
+// renderer
 
 fn render_to_cui(particles: &[Particle]) {
     let mut text = String::new();
@@ -173,9 +195,12 @@ fn render_to_cui(particles: &[Particle]) {
     print!("\x1b[2J\x1b[1;1H{}", text);
 }
 
+// main routine
+
 fn main() {
     let mut rng = rand::thread_rng();
 
+    // spawn particle
     let mut particles = [Particle::default(); PARTICLE_SIZE];
     for i in 0..PARTICLE_SIZE {
         particles[i].position =
