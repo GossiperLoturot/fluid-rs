0f21f7e35b7c3c1150ce8234cb6717fbe0f6e185
Takumi Sugimoto <ts20030211@gmail.com>
2025-09-21 17:09:03 +0900

    add 3d

--- src/3d_multi.rs
+++ src/3d_multi.rs
@@ -4,13 +4,13 @@ struct Config {
     dt: f32,
     iterations: i32,
     grid_res: i32,
-    gravity: Vec2,
+    gravity: Vec3,
     rest_density: f32,
     dynamic_viscosity: f32,
     eos_stiffness: f32,
     eos_power: f32,
     mouse_radius: f32,
-    boundary_clip: (Vec2, Vec2),
+    boundary_clip: (Vec3, Vec3),
     boundary_damp_dist: f32,
 }
 
@@ -19,14 +19,14 @@ impl Default for Config {
         Self {
             dt: 0.032,
             iterations: (1.0 / 0.032) as i32,
-            grid_res: 32,
-            gravity: Vec2::new(0.0, 0.3),
-            rest_density: 4.0,
+            grid_res: 16,
+            gravity: Vec3::new(0.0, 0.3, 0.0),
+            rest_density: 2.0,
             dynamic_viscosity: 0.1,
             eos_stiffness: 10.0,
             eos_power: 4.0,
             mouse_radius: 10.0,
-            boundary_clip: (Vec2::new(0.0, 0.0), Vec2::new(64.0, 64.0)),
+            boundary_clip: (Vec3::new(0.0, 0.0, 0.0), Vec3::new(64.0, 64.0, 64.0)),
             boundary_damp_dist: 3.0,
         }
     }
@@ -34,27 +34,27 @@ impl Default for Config {
 
 #[derive(Debug, Default, Clone, Copy, PartialEq)]
 struct Particle {
-    pos: Vec2,
-    vel: Vec2,
-    affine_momentum: Mat2,
+    pos: Vec3,
+    vel: Vec3,
+    affine_momentum: Mat3,
     mass: f32,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq)]
 struct Cell {
-    vel: Vec2,
+    vel: Vec3,
     mass: f32,
 }
 
 struct Simulation {
     config: Config,
-    particles_mul: ahash::AHashMap<IVec2, Vec<Particle>>,
+    particles_mul: ahash::AHashMap<IVec3, Vec<Particle>>,
     grid_mul: Vec<Cell>,
-    grid_size: IVec2,
+    grid_size: IVec3,
     swap_mul: Vec<Vec<Particle>>,
-    swap_size: IVec2,
-    p_rect: (IVec2, IVec2),
-    a_rect: (IVec2, IVec2),
+    swap_size: IVec3,
+    p_rect: (IVec3, IVec3),
+    a_rect: (IVec3, IVec3),
     debug_elapseds: Vec<(&'static str, std::time::Duration)>,
 }
 
@@ -64,22 +64,22 @@ impl Simulation {
             config,
             particles_mul: ahash::AHashMap::new(),
             grid_mul: Vec::new(),
-            grid_size: IVec2::ZERO,
+            grid_size: IVec3::ZERO,
             swap_mul: Vec::new(),
-            swap_size: IVec2::ZERO,
-            p_rect: (IVec2::ZERO, IVec2::ZERO),
-            a_rect: (IVec2::ZERO, IVec2::ZERO),
+            swap_size: IVec3::ZERO,
+            p_rect: (IVec3::ZERO, IVec3::ZERO),
+            a_rect: (IVec3::ZERO, IVec3::ZERO),
             debug_elapseds: Vec::new(),
         }
     }
 
-    pub fn set_rect(&mut self, min: Vec2, max: Vec2) {
+    pub fn set_rect(&mut self, min: Vec3, max: Vec3) {
         let min_key = key_from_pos(min, &self.config);
-        let max_key = key_from_pos(max, &self.config) + IVec2::ONE;
+        let max_key = key_from_pos(max, &self.config) + IVec3::ONE;
         self.a_rect = (min_key, max_key);
 
-        let min_key = min_key - IVec2::ONE;
-        let max_key = max_key + IVec2::ONE;
+        let min_key = min_key - IVec3::ONE;
+        let max_key = max_key + IVec3::ONE;
         self.p_rect = (min_key, max_key);
 
         // for particles
@@ -89,12 +89,12 @@ impl Simulation {
 
         // for grid
         self.grid_size = (self.p_rect.1 - self.p_rect.0) * self.config.grid_res;
-        let size = self.grid_size.x * self.grid_size.y;
+        let size = self.grid_size.x * self.grid_size.y * self.grid_size.z;
         self.grid_mul = vec![Default::default(); size as usize];
 
         // for move
         self.swap_size = self.p_rect.1 - self.p_rect.0;
-        let size = self.swap_size.x * self.swap_size.y;
+        let size = self.swap_size.x * self.swap_size.y * self.swap_size.z;
         self.swap_mul = vec![Default::default(); size as usize];
     }
 
@@ -132,7 +132,7 @@ impl Simulation {
 
     fn clear_grid(&mut self) {
         for cell in self.grid_mul.iter_mut() {
-            cell.vel = Vec2::ZERO;
+            cell.vel = Vec3::ZERO;
             cell.mass = 0.0;
         }
     }
@@ -142,23 +142,26 @@ impl Simulation {
             let particles = self.particles_mul.get(&k).unwrap();
 
             for p in particles.iter() {
-                let cell_pos = p.pos.floor().as_ivec2();
-                let cell_diff = p.pos - (cell_pos.as_vec2() + Vec2::splat(0.5));
-                let ws = quadratic_weights(cell_diff);
+                let cell_pos = p.pos.floor().as_ivec3();
+                let cell_diff = p.pos - (cell_pos.as_vec3() + Vec3::splat(0.5));
+                let weights = quadratic_weights(cell_diff);
 
-                for n in grid_search(&IVec2::splat(0), &IVec2::splat(3)) {
-                    let cell_pos_n = cell_pos + n - IVec2::ONE;
-                    let cell_diff_n = p.pos - (cell_pos_n.as_vec2() + Vec2::splat(0.5));
-                    let w = ws[n.x as usize].x * ws[n.y as usize].y;
+                for n in grid_search(&IVec3::splat(0), &IVec3::splat(3)) {
+                    let cell_pos_n = cell_pos + n - IVec3::ONE;
+                    let cell_diff_n = p.pos - (cell_pos_n.as_vec3() + Vec3::splat(0.5));
+                    let weight =
+                        weights[n.x as usize].x * weights[n.y as usize].y * weights[n.z as usize].z;
 
                     let q = p.affine_momentum * -cell_diff_n;
-                    let mass_contrib = w * p.mass;
+                    let mass_contrib = weight * p.mass;
 
                     let c1 = cell_pos_n.cmplt(self.p_rect.0 * self.config.grid_res).any();
                     let c2 = cell_pos_n.cmpge(self.p_rect.1 * self.config.grid_res).any();
                     if !(c1 || c2) {
-                        let index_xy = cell_pos_n - self.p_rect.0 * self.config.grid_res;
-                        let index = index_xy.x + index_xy.y * self.grid_size.x;
+                        let index_xyz = cell_pos_n - self.p_rect.0 * self.config.grid_res;
+                        let index = index_xyz.x
+                            + index_xyz.y * self.grid_size.x
+                            + index_xyz.z * self.grid_size.x * self.grid_size.y;
                         let cell_n = self.grid_mul.get_mut(index as usize).unwrap();
                         cell_n.mass += mass_contrib;
                         cell_n.vel += mass_contrib * (p.vel + q);
@@ -173,51 +176,57 @@ impl Simulation {
             let particles = self.particles_mul.get(&k).unwrap();
 
             for p in particles.iter() {
-                let cell_pos = p.pos.floor().as_ivec2();
-                let cell_diff = p.pos - (cell_pos.as_vec2() + Vec2::splat(0.5));
-                let ws = quadratic_weights(cell_diff);
+                let cell_pos = p.pos.floor().as_ivec3();
+                let cell_diff = p.pos - (cell_pos.as_vec3() + Vec3::splat(0.5));
+                let weights = quadratic_weights(cell_diff);
 
                 let rest_density = self.config.rest_density;
                 let eos_stiffness = self.config.eos_stiffness;
                 let eos_power = self.config.eos_power;
 
                 let mut density = 0.0;
-                for n in grid_search(&IVec2::splat(0), &IVec2::splat(3)) {
-                    let cell_pos_n = cell_pos + n - IVec2::ONE;
-                    let w = ws[n.x as usize].x * ws[n.y as usize].y;
+                for n in grid_search(&IVec3::splat(0), &IVec3::splat(3)) {
+                    let cell_pos_n = cell_pos + n - IVec3::ONE;
+                    let weight =
+                        weights[n.x as usize].x * weights[n.y as usize].y * weights[n.z as usize].z;
 
                     let c1 = cell_pos_n.cmplt(self.p_rect.0 * self.config.grid_res).any();
                     let c2 = cell_pos_n.cmpge(self.p_rect.1 * self.config.grid_res).any();
                     if !(c1 || c2) {
-                        let index_xy = cell_pos_n - self.p_rect.0 * self.config.grid_res;
-                        let index = index_xy.x + index_xy.y * self.grid_size.x;
+                        let index_xyz = cell_pos_n - self.p_rect.0 * self.config.grid_res;
+                        let index = index_xyz.x
+                            + index_xyz.y * self.grid_size.x
+                            + index_xyz.z * self.grid_size.x * self.grid_size.y;
                         let cell_n = self.grid_mul.get(index as usize).unwrap();
-                        density += cell_n.mass * w;
+                        density += cell_n.mass * weight;
                     }
                 }
                 let volume = p.mass / density;
                 let pressure = f32::max(
-                    -0.0,
+                    -0.1,
                     eos_stiffness * ((density / rest_density).powf(eos_power) - 1.0),
                 );
 
                 let strain = p.affine_momentum + p.affine_momentum.transpose();
                 let viscosity_term = self.config.dynamic_viscosity * strain;
-                let stress = -pressure * Mat2::IDENTITY + viscosity_term;
+                let stress = -pressure * Mat3::IDENTITY + viscosity_term;
                 let eg_16_term_0 = -4.0 * volume * stress * self.config.dt;
 
-                for n in grid_search(&IVec2::splat(0), &IVec2::splat(3)) {
-                    let cell_pos_n = cell_pos + n - IVec2::ONE;
-                    let cell_diff_n = p.pos - (cell_pos_n.as_vec2() + Vec2::splat(0.5));
-                    let w = ws[n.x as usize].x * ws[n.y as usize].y;
+                for n in grid_search(&IVec3::splat(0), &IVec3::splat(3)) {
+                    let cell_pos_n = cell_pos + n - IVec3::ONE;
+                    let cell_diff_n = p.pos - (cell_pos_n.as_vec3() + Vec3::splat(0.5));
+                    let weight =
+                        weights[n.x as usize].x * weights[n.y as usize].y * weights[n.z as usize].z;
 
                     let c1 = cell_pos_n.cmplt(self.p_rect.0 * self.config.grid_res).any();
                     let c2 = cell_pos_n.cmpge(self.p_rect.1 * self.config.grid_res).any();
                     if !(c1 || c2) {
-                        let index_xy = cell_pos_n - self.p_rect.0 * self.config.grid_res;
-                        let index = index_xy.x + index_xy.y * self.grid_size.x;
+                        let index_xyz = cell_pos_n - self.p_rect.0 * self.config.grid_res;
+                        let index = index_xyz.x
+                            + index_xyz.y * self.grid_size.x
+                            + index_xyz.z * self.grid_size.x * self.grid_size.y;
                         let cell_n = self.grid_mul.get_mut(index as usize).unwrap();
-                        cell_n.vel += w * eg_16_term_0 * -cell_diff_n;
+                        cell_n.vel += weight * eg_16_term_0 * -cell_diff_n;
                     }
                 }
             }
@@ -238,29 +247,33 @@ impl Simulation {
             let particles = self.particles_mul.get_mut(&k).unwrap();
 
             for p in particles.iter_mut() {
-                p.vel = Vec2::ZERO;
+                p.vel = Vec3::ZERO;
 
-                let cell_pos = p.pos.floor().as_ivec2();
-                let cell_diff = p.pos - (cell_pos.as_vec2() + Vec2::splat(0.5));
-                let ws = quadratic_weights(cell_diff);
+                let cell_pos = p.pos.floor().as_ivec3();
+                let cell_diff = p.pos - (cell_pos.as_vec3() + Vec3::splat(0.5));
+                let weights = quadratic_weights(cell_diff);
 
-                let mut b_mat = Mat2::ZERO;
-                for n in grid_search(&IVec2::splat(0), &IVec2::splat(3)) {
-                    let cell_pos_n = cell_pos + n - IVec2::ONE;
-                    let cell_diff_n = p.pos - (cell_pos_n.as_vec2() + Vec2::splat(0.5));
-                    let w = ws[n.x as usize].x * ws[n.y as usize].y;
+                let mut b_mat = Mat3::ZERO;
+                for n in grid_search(&IVec3::splat(0), &IVec3::splat(3)) {
+                    let cell_pos_n = cell_pos + n - IVec3::ONE;
+                    let cell_diff_n = p.pos - (cell_pos_n.as_vec3() + Vec3::splat(0.5));
+                    let weight =
+                        weights[n.x as usize].x * weights[n.y as usize].y * weights[n.z as usize].z;
 
                     let c1 = cell_pos_n.cmplt(self.p_rect.0 * self.config.grid_res).any();
                     let c2 = cell_pos_n.cmpge(self.p_rect.1 * self.config.grid_res).any();
                     if !(c1 || c2) {
-                        let index_xy = cell_pos_n - self.p_rect.0 * self.config.grid_res;
-                        let index = index_xy.x + index_xy.y * self.grid_size.x;
+                        let index_xyz = cell_pos_n - self.p_rect.0 * self.config.grid_res;
+                        let index = index_xyz.x
+                            + index_xyz.y * self.grid_size.x
+                            + index_xyz.z * self.grid_size.x * self.grid_size.y;
                         let cell_n = self.grid_mul.get(index as usize).unwrap();
-                        let weighted_velocity = cell_n.vel * w;
+                        let weighted_velocity = cell_n.vel * weight;
 
-                        let term = Mat2::from_cols(
+                        let term = Mat3::from_cols(
                             weighted_velocity * -cell_diff_n.x,
                             weighted_velocity * -cell_diff_n.y,
+                            weighted_velocity * -cell_diff_n.z,
                         );
                         b_mat += term;
                         p.vel += weighted_velocity;
@@ -273,15 +286,15 @@ impl Simulation {
                 // mouse interaction
 
                 if let Some(mouse) = mouse_pos {
-                    let dist = p.pos - mouse;
+                    let dist = p.pos.xy() - mouse;
                     if dist.length_squared() < self.config.mouse_radius * self.config.mouse_radius {
-                        p.vel += dist.normalize_or_zero();
+                        p.vel += Vec3::ZERO.with_xy(dist.normalize_or_zero());
                     }
                 }
 
                 // boundary conditions
 
-                p.pos = Vec2::clamp(
+                p.pos = Vec3::clamp(
                     p.pos,
                     self.config.boundary_clip.0,
                     self.config.boundary_clip.1,
@@ -289,9 +302,9 @@ impl Simulation {
 
                 let next_pos = p.pos + p.vel;
                 let wall_min =
-                    self.config.boundary_clip.0 + Vec2::splat(self.config.boundary_damp_dist);
+                    self.config.boundary_clip.0 + Vec3::splat(self.config.boundary_damp_dist);
                 let wall_max =
-                    self.config.boundary_clip.1 - Vec2::splat(self.config.boundary_damp_dist);
+                    self.config.boundary_clip.1 - Vec3::splat(self.config.boundary_damp_dist);
 
                 if next_pos.x < wall_min.x {
                     p.vel.x += wall_min.x - next_pos.x;
@@ -305,6 +318,12 @@ impl Simulation {
                 if next_pos.y > wall_max.y {
                     p.vel.y += wall_max.y - next_pos.y;
                 }
+                if next_pos.z < wall_min.z {
+                    p.vel.z += wall_min.z - next_pos.z;
+                }
+                if next_pos.z > wall_max.z {
+                    p.vel.z += wall_max.z - next_pos.z;
+                }
             }
 
             // swap
@@ -316,8 +335,10 @@ impl Simulation {
                 let c1 = k.cmplt(self.p_rect.0).any();
                 let c2 = k.cmpge(self.p_rect.1).any();
                 if !(c1 || c2) {
-                    let index_xy = k - self.p_rect.0;
-                    let index = index_xy.x + index_xy.y * self.swap_size.x;
+                    let index_xyz = k - self.p_rect.0;
+                    let index = index_xyz.x
+                        + index_xyz.y * self.swap_size.x
+                        + index_xyz.z * self.swap_size.x * self.swap_size.y;
                     let r#move = self.swap_mul.get_mut(index as usize).unwrap();
                     r#move.push(p);
                 }
@@ -325,8 +346,10 @@ impl Simulation {
             for k in grid_search(&self.p_rect.0, &self.p_rect.1) {
                 let particles = self.particles_mul.get_mut(&k).unwrap();
 
-                let index_xy = k - self.p_rect.0;
-                let index = index_xy.x + index_xy.y * self.swap_size.x;
+                let index_xyz = k - self.p_rect.0;
+                let index = index_xyz.x
+                    + index_xyz.y * self.swap_size.x
+                    + index_xyz.z * self.swap_size.x * self.swap_size.y;
                 let r#move = self.swap_mul.get_mut(index as usize).unwrap();
 
                 particles.append(r#move);
@@ -336,12 +359,12 @@ impl Simulation {
 
     pub fn iter_particle(&self) -> impl Iterator<Item = &Particle> + '_ {
         grid_search(&self.a_rect.0, &self.a_rect.1)
-            .filter_map(|key| self.particles_mul.get(&key))
+            .filter_map(|k| self.particles_mul.get(&k))
             .flatten()
     }
 }
 
-fn quadratic_weights(cell_diff: Vec2) -> [Vec2; 3] {
+fn quadratic_weights(cell_diff: Vec3) -> [Vec3; 3] {
     [
         0.5 * (0.5 - cell_diff) * (0.5 - cell_diff),
         0.75 - cell_diff * cell_diff,
@@ -349,15 +372,16 @@ fn quadratic_weights(cell_diff: Vec2) -> [Vec2; 3] {
     ]
 }
 
-fn key_from_pos(pos: Vec2, config: &Config) -> IVec2 {
-    pos.div_euclid(Vec2::splat(config.grid_res as f32))
-        .as_ivec2()
+fn key_from_pos(pos: Vec3, config: &Config) -> IVec3 {
+    pos.div_euclid(Vec3::splat(config.grid_res as f32))
+        .as_ivec3()
 }
 
-fn grid_search(min: &IVec2, max: &IVec2) -> impl Iterator<Item = IVec2> {
-    (min.y..max.y)
-        .flat_map(|y| (min.x..max.x).map(move |x| (x, y)))
-        .map(|(x, y)| IVec2::new(x, y))
+fn grid_search(min: &IVec3, max: &IVec3) -> impl Iterator<Item = IVec3> {
+    (min.z..max.z)
+        .flat_map(|z| (min.y..max.y).map(move |y| (y, z)))
+        .flat_map(|(y, z)| (min.x..max.x).map(move |x| (x, y, z)))
+        .map(|(x, y, z)| IVec3::new(x, y, z))
 }
 
 #[derive(Clone, Copy)]
@@ -423,7 +447,7 @@ fn draw(
     let mut bin_counts = vec![0; bin_count_size as usize];
 
     for p in sim.iter_particle() {
-        let console_xy = (p.pos / viewport_size * console_size.as_vec2()).as_ivec2();
+        let console_xy = (p.pos.xy() / viewport_size * console_size.as_vec2()).as_ivec2();
 
         if console_xy.cmplt(IVec2::ZERO).any() || console_xy.cmpge(console_size).any() {
             continue;
@@ -477,16 +501,17 @@ fn main() -> std::io::Result<()> {
     let mut rng = rand::rng();
     for _ in 0..4096 {
         sim.add_particle(Particle {
-            pos: Vec2::new(
-                rand::Rng::random_range(&mut rng, 16.0..=48.0),
-                rand::Rng::random_range(&mut rng, 16.0..=48.0),
+            pos: Vec3::new(
+                rand::Rng::random_range(&mut rng, 16.0..=32.0),
+                rand::Rng::random_range(&mut rng, 16.0..=32.0),
+                rand::Rng::random_range(&mut rng, 16.0..=32.0),
             ),
-            vel: Vec2::ZERO,
-            affine_momentum: Mat2::ZERO,
+            vel: Vec3::ZERO,
+            affine_momentum: Mat3::ZERO,
             mass: 1.0,
         });
     }
-    sim.set_rect(Vec2::new(0.0, 0.0), Vec2::new(64.0, 64.0));
+    sim.set_rect(Vec3::new(0.0, 0.0, 0.0), Vec3::new(64.0, 64.0, 64.0));
 
     let viewport_size = Vec2::new(64.0, 64.0);
     let console_size = IVec2::new(80, 40);
